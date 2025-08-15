00ad4ce92ddf99dfd79f1ba4c538b9680bc18022
Ganesh23105 <146025072+Ganesh23105@users.noreply.github.com>
2025-08-15 15:56:05 +0530

    Time: 5 ms (24.20%) | Memory: 46.4 MB (12.10%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,36 +1,36 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        ArrayList<Integer> list=new ArrayList<>();
-        for(int num:nums1){
-            list.add(num);
-        }
-        for(int num:nums2){
-            list.add(num);
-        }
-        Collections.sort(list);
-
-        int n=list.size();
-
-        if(n%2==0){
-            return ((list.get((n/2)-1))+list.get(n/2))/2.0;
-        }
-        else{
-            return list.get(n/2);
-        }
-        // int n=nums1.length;
-        // int m=nums2.length;
-        // int merged[]=new int[m+n];
-        // System.arraycopy(nums1,0,merged,0,n);
-        // System.arraycopy(nums2,0,merged,n,m);
-        // Arrays.sort(merged);
-        // int target=merged.length;
-        // if(target%2==1){
-        //     return merged[target/2];
-        // }else{
-        //     int mid1=merged[target/2];
-        //     int mid2=merged[(target/2)-1];
-        //     return (mid1+mid2)/2.0;
+        // ArrayList<Integer> list=new ArrayList<>();
+        // for(int num:nums1){
+        //     list.add(num);
         // }
+        // for(int num:nums2){
+        //     list.add(num);
+        // }
+        // Collections.sort(list);
+
+        // int n=list.size();
+
+        // if(n%2==0){
+        //     return ((list.get((n/2)-1))+list.get(n/2))/2.0;
+        // }
+        // else{
+        //     return list.get(n/2);
+        // }
+        int n=nums1.length;
+        int m=nums2.length;
+        int merged[]=new int[m+n];
+        System.arraycopy(nums1,0,merged,0,n);
+        System.arraycopy(nums2,0,merged,n,m);
+        Arrays.sort(merged);
+        int target=merged.length;
+        if(target%2==1){
+            return merged[target/2];
+        }else{
+            int mid1=merged[target/2];
+            int mid2=merged[(target/2)-1];
+            return (mid1+mid2)/2.0;
+        }
     }
 
 }
