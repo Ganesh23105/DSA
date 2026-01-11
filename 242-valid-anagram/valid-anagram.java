a6943b33d7ce5e88c604aa0c4850a8c266011929
Ganesh23105 <146025072+Ganesh23105@users.noreply.github.com>
2026-01-11 13:05:19 +0530

    Time: 7 ms (32.80%) | Memory: 46.8 MB (6.14%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,20 +3,54 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        int[] freq=new int[26];
-        String S=s.replaceAll("\\s","").toLowerCase();
-        String T=t.replaceAll("\\s","").toLowerCase();
 
-        for(int i=0;i<S.length();i++){
-            freq[S.charAt(i)-'a']++;
-            freq[T.charAt(i)-'a']--;
-        }
+        String S=s.replaceAll("\\s","");
+        String T=t.replaceAll("\\s","");
 
-        for(int i=0;i<freq.length;i++){
-            if(freq[i]!=0){
-                return false;
-            }
-        }
-        return true;
+        char[] string1=S.toCharArray();
+        char[] string2=T.toCharArray();
+
+        Arrays.sort(string1);
+        Arrays.sort(string2);
+        return Arrays.equals(string1,string2);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         if(s.length()!=t.length()){
+//             return false;
+//         }
+//         int[] freq=new int[26];
+//         String S=s.replaceAll("\\s","").toLowerCase();
+//         String T=t.replaceAll("\\s","").toLowerCase();
+
+//         for(int i=0;i<S.length();i++){
+//             freq[S.charAt(i)-'a']++;
+//             freq[T.charAt(i)-'a']--;
+//         }
+
+//         for(int i=0;i<freq.length;i++){
+//             if(freq[i]!=0){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
