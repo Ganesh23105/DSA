eafff5bdd1e37ca0af9d568368fc5ad04f09d92d
Ganesh23105 <146025072+Ganesh23105@users.noreply.github.com>
2026-01-14 23:43:12 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (9.78%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.java
+++ 1755-defuse-the-bomb/defuse-the-bomb.java
@@ -1,31 +1,36 @@
 class Solution {
     public int[] decrypt(int[] code, int k) {
-        int n=code.length;
-        int [] ans=new int[n];
+        int n = code.length;
+        int[] ans = new int[n];
 
-        if(k==0){
-            return ans;
-        }
+        if (k == 0) return ans;
 
-        for(int i=0;i<n;i++){
-            int sum=0;
+        int sum = 0;
 
-            if(k>0){
-                for(int j=1;j<=k;j++){
-                    sum+=code[(i+j)%n];
-                }
+        if (k > 0) {
+            for (int i = 1; i <= k; i++) {
+                sum += code[i % n];
             }
 
-            if(k<0){
-                for(int j=1;j<=-k;j++){
-                    sum+=code[(i-j+n)%n];
-                }
+            for (int i = 0; i < n; i++) {
+                ans[i] = sum;
+                sum -= code[(i + 1) % n];
+                sum += code[(i + k + 1) % n];
             }
 
-            ans[i]=sum;
+        } else {
+            k = -k;
+            for (int i = 1; i <= k; i++) {
+                sum += code[(n - i) % n];
+            }
+
+            for (int i = 0; i < n; i++) {
+                ans[i] = sum;
+                sum -= code[(n - k + i) % n];
+                sum += code[i];
+            }
         }
 
         return ans;
-        
     }
 }
