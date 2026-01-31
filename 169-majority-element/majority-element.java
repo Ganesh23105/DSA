753f0759f3591356501f05e752587a3a1a1e8e0a
Ganesh23105 <146025072+Ganesh23105@users.noreply.github.com>
2026-02-01 00:29:43 +0530

    Time: 1 ms (99.82%) | Memory: 55.8 MB (42.32%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,27 +1,46 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> result=new HashMap<>();
-        // for(int i=0;i<nums.length;i++){
-        //     if(result.containsKey(nums[i])){
-        //         result.put(nums[i],result.get(nums[i])+1);
-        //     }
-        //     else{
-        //        result.put(nums[i],1);
+        // HashMap<Integer,Integer> result=new HashMap<>();
+        // int majority=nums[0];
+        // int maxcount=0;
+
+        // for(int x:nums){
+        //     result.put(x,result.getOrDefault(x,0)+1);
+
+        //     if(result.get(x)>maxcount && result.get(x)>nums.length/2){
+        //         maxcount=result.get(x);
+        //         majority=x;
         //     }
         // }
+        // return majority;
 
-        int majority=nums[0];
-        int maxcount=0;
+        int count=0;
+        int element=0;
 
-        for(int x:nums){
-            result.put(x,result.getOrDefault(x,0)+1);
-
-            if(result.get(x)>maxcount && result.get(x)>nums.length/2){
-                maxcount=result.get(x);
-                majority=x;
+        for(int i=0;i<nums.length;i++){
+            if(count==0){
+                count=1;
+                element=nums[i];
+            }
+            else if(nums[i]==element){
+                count++;
+            }
+            else{
+                count--;
             }
         }
-        return majority;
+        int count1=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==element){
+                count1++;
+            }
+        }
+
+        if(count1>nums.length/2){
+            return element;
+        }
+
+        return -1;
         
     }
 }
