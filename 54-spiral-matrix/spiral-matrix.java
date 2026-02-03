6edaa80c97a58f7c07570a16d71b1312ba56604a
Ganesh23105 <146025072+Ganesh23105@users.noreply.github.com>
2026-02-03 10:53:04 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (87.78%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -3,42 +3,41 @@ class Solution {
         List<Integer> result=new ArrayList<>();
         int n=matrix.length;
         int m=matrix[0].length;
-
-        int top=0;
-        int bottom=n-1;
         int left=0;
+        int top=0;
         int right=m-1;
+        int bottom=n-1;
 
-        while(top<=bottom && left<=right){
+        while(left<=right && top<=bottom){
 
-            // Right
-            for(int i=left;i<=right;i++){
-                result.add(matrix[top][i]);
-            }
-            top++;
+        for(int i=left;i<=right;i++){
+            result.add(matrix[top][i]);
+        }
+        top++;
 
-            for(int i=top;i<=bottom;i++){
-                result.add(matrix[i][right]);
-            }
-            right--;
+        for(int i=top;i<=bottom;i++){
+            result.add(matrix[i][right]);
+        }
+        right--;
 
-            if(top<=bottom){
-                for(int i=right;i>=left;i--){
-                result.add(matrix[bottom][i]);
+        if(top<=bottom){
+            for(int i=right;i>=left;i--)
+            {
+            result.add(matrix[bottom][i]);
             }
             bottom--;
-
-            }
-            
-            if(left<=right){
-            for(int i=bottom;i>=top;i--){
-                result.add(matrix[i][left]);
+        }
+        
+        if(left<=right){
+            for(int i=bottom;i>=top;i--)
+            {
+            result.add(matrix[i][left]);
             }
             left++;
-            }
-
         }
 
+        }
         return result;
+        
     }
 }
